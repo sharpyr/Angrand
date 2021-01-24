d87ff4e3a5da4f9e220e0383c494f39dd57ef721
hoyeungw <hoyeungw@outlook.com>
2021-01-24 14:45:00 +0800

    minor fix

--- Angrand.UserControls.PalettControl/Properties/AssemblyInfo.cs
+++ Angrand.UserControls.PalettControl/Properties/AssemblyInfo.cs
@@ -6,18 +6,18 @@ using System.Runtime.InteropServices;
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("Angrand.UserControls.PalettControl")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("color picker configurable in rgb and hsl")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Hoyeung Wong")]
 [assembly: AssemblyProduct("Angrand.UserControls.PalettControl")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCopyright("Copyright © Hoyeung Wong  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // 将 ComVisible 设置为 false 会使此程序集中的类型
 //对 COM 组件不可见。如果需要从 COM 访问此程序集中的类型
 //请将此类型的 ComVisible 特性设置为 true。
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
 [assembly: Guid("c3f9bbdd-5ecc-41ed-a7da-c9c47ff7b942")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值
 //通过使用 "*"，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
