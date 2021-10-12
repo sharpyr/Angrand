6b725c418ae429763aa99bc47db54e7596355155
hoyeungw <hoyeungw@outlook.com>
2021-10-13 02:44:11 +0800

    add SplitPalett.cs

---
+++ Angrand.GUI.PalettControl/SplitPalett.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Forms;
+
+namespace Angrand.GUI.PalettControl {
+  public partial class SplitPalett : UserControl {
+    public SplitPalett() {
+      InitializeComponent();
+    }
+  }
+}
