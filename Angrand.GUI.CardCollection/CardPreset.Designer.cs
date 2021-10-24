ecba442fffa44d8b664f9298e2c1f917b70f13f1
hoyeungw <hoyeungw@outlook.com>
2021-10-24 08:53:29 +0800

    add CardPreset

---
+++ Angrand.GUI.CardCollection/CardPreset.Designer.cs
@@ -0,0 +1,69 @@
+﻿using System.ComponentModel;
+
+namespace Angrand.GUI.CardCollection {
+  partial class CardPreset {
+    /// <summary> 
+    /// Required designer variable.
+    /// </summary>
+    private IContainer components = null;
+
+    /// <summary> 
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing) {
+      if (disposing && (components != null)) {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Component Designer generated code
+
+    /// <summary> 
+    /// Required method for Designer support - do not modify 
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent() {
+            this.tableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel
+            // 
+            this.tableLayoutPanel.ColumnCount = 5;
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel.Margin = new System.Windows.Forms.Padding(1);
+            this.tableLayoutPanel.Name = "tableLayoutPanel";
+            this.tableLayoutPanel.Padding = new System.Windows.Forms.Padding(1);
+            this.tableLayoutPanel.RowCount = 1;
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel.Size = new System.Drawing.Size(160, 24);
+            this.tableLayoutPanel.TabIndex = 0;
+            // 
+            // CardPreset
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.Controls.Add(this.tableLayoutPanel);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.Margin = new System.Windows.Forms.Padding(0);
+            this.Name = "CardPreset";
+            this.Size = new System.Drawing.Size(160, 24);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.CardPreset_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.CardPreset_DragEnter);
+            this.ResumeLayout(false);
+
+    }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel;
+  }
+}
---
+++ Angrand.GUI.CardCollection/CardPreset.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using Palett;
+using Palett.Projector;
+using Veho;
+using Veho.Sequence;
+using ColorPair = System.ValueTuple<System.Drawing.Color, System.Drawing.Color>;
+
+namespace Angrand.GUI.CardCollection {
+  public partial class CardPreset : UserControl {
+    public event EventHandler OnLabelDoubleClicked;
+    [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+    [Browsable(false)]
+    public int Count { get; set; } = 5;
+    private readonly Label[] labelCollection;
+    public CardPreset() {
+      InitializeComponent();
+      this.tableLayoutPanel.SuspendLayout();
+      this.SuspendLayout();
+      this.labelCollection = Vec.Init(Count, i => {
+        var label = new Label {
+                                BackColor = Color.FromArgb(50 + i * 50, 50 + i * 50, 50 + i * 50),
+                                Dock = DockStyle.Fill,
+                                FlatStyle = FlatStyle.Flat,
+                                ForeColor = Color.Transparent,
+                                BorderStyle = BorderStyle.None,
+                                Location = new Point(1, 1),
+                                Margin = new Padding(0),
+                                Name = "label_" + i,
+                                Size = new Size(24, 24),
+                                TabIndex = i,
+                              };
+        label.MouseDown += this.label_MouseDown;
+        label.DoubleClick += this.label_DoubleClicked;
+        this.tableLayoutPanel.Controls.Add(label, i, 0);
+        return label;
+      });
+      this.tableLayoutPanel.ResumeLayout(false);
+      this.ResumeLayout(false);
+    }
+
+    public Label LabelLeft => this.labelCollection.First();
+    public Label LabelRight => this.labelCollection.Last();
+
+    [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+    [Browsable(false)]
+    public ColorPair Preset {
+      get => (this.LabelLeft.BackColor, this.LabelRight.BackColor);
+      set {
+        var (min, max) = value;
+        var preset = Palett.Types.Preset.Build(min.ColorToHex(), max.ColorToHex());
+        var projector = ProjectorFactory.Build((0, this.Count - 1), preset);
+        var colorCollection = Vec.Init(this.Count, i => projector.Project(i).HslToColor());
+        labelCollection.IterZip(colorCollection, (label, color) => label.BackColor = color);
+      }
+    }
+
+    private void label_DoubleClicked(object sender, EventArgs e) {
+      this.OnLabelDoubleClicked?.Invoke(this, e);
+    }
+
+    private void label_MouseDown(object sender, MouseEventArgs e) {
+      if (e.Button == MouseButtons.Left && e.Clicks == 1) {
+        var preset = (this.LabelLeft.BackColor, this.LabelRight.BackColor);
+        this.DoDragDrop(preset, DragDropEffects.Copy | DragDropEffects.Move);
+      }
+    }
+
+    private void CardPreset_DragEnter(object sender, DragEventArgs e) {
+      e.Effect = e.Data.GetDataPresent(typeof(ColorPair))
+        ? DragDropEffects.Copy
+        : DragDropEffects.None;
+    }
+
+    private void CardPreset_DragDrop(object sender, DragEventArgs e) {
+      var control = (CardPreset)sender;
+      var (min, max) = (ColorPair)e.Data.GetData(typeof(ColorPair));
+      control.Preset = (min, max);
+    }
+  }
+}
