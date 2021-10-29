9300db78622c30904abfa109a1a1bf928a63f9eb
hoyeungw <hoyeungw@outlook.com>
2021-10-29 10:57:10 +0800

    simplified Util codes

---
+++ Angrand.GUI.CardCollection/Utils.cs
@@ -0,0 +1,12 @@
+﻿using Aryth;
+
+namespace Angrand.GUI.CardCollection {
+  public static class ColorExt {
+    public static (float h, float s, float l) Restrict(this (float h, float s, float l) hsl) {
+      hsl.h = Math.Restrict(hsl.h, 360);
+      hsl.s = Math.Limit(hsl.s, 100);
+      hsl.l = Math.Limit(hsl.l, 100);
+      return hsl;
+    }
+  }
+}
