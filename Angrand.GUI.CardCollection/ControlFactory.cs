23853ac3b2bc92ab7d7ea771e045689d26c4b551
hoyeungw <hoyeungw@outlook.com>
2021-10-29 10:56:00 +0800

    add ControlFactory to create customized controls

---
+++ Angrand.GUI.CardCollection/ControlFactory.cs
@@ -0,0 +1,26 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace Angrand.GUI.CardCollection {
+  public static class ButtonUtil {
+    private static readonly Padding buttonPadding = new Padding(1, 0, 1, 0);
+    
+    public static Button MakePixelButton() {
+      var button = new Button {
+                                ForeColor = Color.Transparent,
+                                TextAlign = ContentAlignment.MiddleCenter,
+                                Padding = buttonPadding,
+                                FlatAppearance = { BorderSize = 0 },
+                                FlatStyle = FlatStyle.Flat,
+                                Location = new Point(0, 0),
+                                Margin = new Padding(0),
+                                Size = new Size(20, 20),
+                                Dock = DockStyle.Fill,
+                                // TabIndex = i * w + j,
+                                UseVisualStyleBackColor = false,
+                                AllowDrop = true
+                              };
+      return button;
+    }
+  }
+}
